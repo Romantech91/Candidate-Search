2d97e3fb6bf4f49d14ba67c9d34540c69d742aeb
Victor Roman <Romantech91>
2024-09-08 15:50:58 -0700

    saved candidates component completed

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,7 +1,41 @@
+import type React from 'react';
+import { useEffect, useState } from 'react';
+import { Candidate } from '../interface/Candidate';
+
+
 const SavedCandidates = () => {
+  const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
+   useEffect(() => {
+    const savedCandidates = localStorage.getItem('savedCandidates');
+    if (savedCandidates) {
+      setSavedCandidates(JSON.parse(savedCandidates));
+    }
+  }, []);
+
+  const removeFromSavedCandidates = (id: number) => {
+    const updatedCandidates = savedCandidates.filter((candidate) => candidate.id !== id);
+    setSavedCandidates(updatedCandidates);
+    localStorage.setItem('savedCandidates', JSON.stringify(updatedCandidates));
+  };
   return (
     <>
       <h1>Potential Candidates</h1>
+      {savedCandidates.length > 0 ? (
+      <ul>
+        {savedCandidates.map((candidate) => (
+          <li key={candidate.id}>
+            <img src={candidate.avatar_url} alt={`${candidate.login}'s avatar`} />
+            <h2>{candidate.login}</h2>
+            <a href={candidate.html_url} target="_blank" rel="noopener noreferrer">
+              View Profile
+            </a>
+            <button onClick={() => removeFromSavedCandidates(candidate.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      ) : (
+        <p>No saved candidates</p>
+      )}  
     </>
   );
 };
